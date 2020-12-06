d4486df8593afcdbee0b8158875e094b98555be1
QiTao Weng <q350w358@ku.edu>
2020-12-06 15:18:05 -0600

    Fixes to createUser syntax and remove unneccesary code once set user_id as unique key in database

--- createUser.php
+++ createUser.php
@@ -10,19 +10,14 @@ if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-$query = "SELECT user_id FROM Users WHERE user_id = '$username'";
-$new_user = "INSERT INTO Users (user_id)
-        VALUES ('".$username."')";
-$result =  $mysqli->query($query);
-if (!$result) {
-    if ($mysqli->query($new_user) === TRUE) {
-        echo "New user created successfully";
-    } else {
-        echo "Error: " . $new_user . "<br>" . $mysqli->error;
-    }
+$new_user = "INSERT INTO Users (user_id) 
+            VALUES ('".$username."')";
+if ($mysqli->query($new_user) === TRUE) {
+    echo "New user created successfully";
 } else {
-    echo "Error: user already exists";
+    echo "Error: " . $new_user . "<br>" . $mysqli->error;
 }
+
 
 /* close connection */
 $mysqli->close();
